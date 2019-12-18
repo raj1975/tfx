9b8a2e33dd91815c496a7f2f343dedd3269541ad
yanhuasun <yanhuasun@google.com>
2019-12-18 09:17:40 -0800

    Explicitly replace "import tensorflow" with "tensorflow.compat.v1"
    PiperOrigin-RevId: 286205200

--- tfx/components/base/base_driver_test.py
+++ tfx/components/base/base_driver_test.py
@@ -21,6 +21,7 @@ from __future__ import print_function
 import os
 import mock
 import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from ml_metadata.proto import metadata_store_pb2
 from tfx import types
 from tfx.components.base import base_driver
